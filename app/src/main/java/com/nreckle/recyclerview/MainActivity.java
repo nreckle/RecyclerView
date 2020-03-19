9d1a7d085aec13816a2f77cbc2159b53ab932c89
zhaopengchao <zhaopengchao-os@360os.com>
2020-03-19 11:38:17 +0800

    Init recyclerview project

---
+++ app/src/main/java/com/nreckle/recyclerview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.nreckle.recyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mMainRecyclerView;
+    private MainAdapter mMainAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] mDataSet = {"adfafda", "afafdafefa", "geafdafae",
+            "eteareafadsfafa", "meiwnofenaofe", "zeafeljo",
+            "afdafae", "tereafadsfafa", "wnofenaofe", "zeafeljo",
+            "geafdafae", "etearfadsfafa", "meiofenaofe", "eljo",
+            "geaffae", "etearefadsfafa", "meiwfenaofe", "zeaeljo",
+            "geafae", "eareafadsfafa", "meiofenaofe", "zefeljo"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainRecyclerView = findViewById(R.id.rv_main_content);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mMainRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mMainRecyclerView.setLayoutManager(mLayoutManager);
+
+        mMainAdapter = new MainAdapter(mDataSet);
+        mMainRecyclerView.setAdapter(mMainAdapter);
+    }
+}
---
+++ app/src/main/java/com/nreckle/recyclerview/MainAdapter.java
@@ -0,0 +1,48 @@
+package com.nreckle.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> {
+
+    private String[] mDataSet;
+
+    public MainAdapter(String[] dataSet) {
+        mDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public final TextView textView;
+
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+}
